15ec7def9b8acaebb7e3b837725ed59ddcf0a06d
huytruengusi <99071294+huytruengusi@users.noreply.github.com>
2022-04-19 10:18:33 +0700

    19/4/2022 10:18

--- tictactoe/demo/src/main/java/com/example/App.java
+++ tictactoe/demo/src/main/java/com/example/App.java
@@ -2,31 +2,27 @@ package com.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Timer;
 import java.util.TimerTask;
+
 public class App 
 {
     private static int sec = 0;
+    private static JLabel lblTime;
+    private static Timer timer = new Timer();
+    private static JButton btnStart;
+    private static Board board;
     public static void main( String[] args )
     {
-        //Time
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            public void run(){
-                sec++;
-                System.out.println(sec/60 + " : " + sec%60);
-            }
-        }, 1000, 1000);
-        // timer.cancel();
-        // Yes or No Board
-        int choice = JOptionPane.showConfirmDialog(null, "Người chơi O đi trước", "Ai là người đi trước?", JOptionPane.YES_NO_OPTION);
-        Board board = new Board(choice == 1 ? Cell.X_VALUE : Cell.O_VALUE);
         //jPanel
         JPanel jPanel = new JPanel();
         BoxLayout boxLayout = new BoxLayout(jPanel, BoxLayout.Y_AXIS);
         jPanel.setLayout(boxLayout);
         
         //Board
+        board = new Board();
         board.setPreferredSize(new Dimension(300,300));
         
         //Flow Layout
@@ -37,19 +33,24 @@ public class App
         bottomPanel.setLayout(flowLayout);
 
         //btnStart
-        JButton btnStart = new JButton("Start");
+        btnStart = new JButton("Start");
         
         // lblTime
-        JLabel lblTime = new JLabel("00:00");
-
+        lblTime = new JLabel("00:00");
+        
         bottomPanel.add(lblTime);
         bottomPanel.add(btnStart);
-
-        jPanel.add(board);
-        jPanel.add(bottomPanel);
-
+        btnStart.addActionListener(new ActionListener(){
+            public void actionPerformed (ActionEvent e){
+                if (btnStart.getText().equals("Start")){
+                    startGame();
+                }else{
+                    stopGame();
+                }
+            }
+        });
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-
+        
         JFrame jFrame = new JFrame("TIC TAC TOE");
         jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
         jFrame.setResizable(true);
@@ -62,5 +63,23 @@ public class App
         jFrame.setLocation(x, y);
         jFrame.pack();
         jFrame.setVisible(true);
+    }
+    private static void startGame(){
+        // Yes or No Board
+        int choice = JOptionPane.showConfirmDialog(null, "Người chơi O đi trước", "Ai là người đi trước?", JOptionPane.YES_NO_OPTION);
+        Board board = new Board(choice == 1 ? Cell.X_VALUE : Cell.O_VALUE);
+        // BtnStar
+
+        lblTime.setText("00:00");
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run(){
+                sec++;
+                String value = sec/60 + " : " + sec%60 ;
+                lblTime.setText(value);
+            }
+        }, 1000, 1000);
+    }
+    private static void stopGame(){
+
     }
 }
